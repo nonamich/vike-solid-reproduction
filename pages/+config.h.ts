e90907142e66af79f2695226fb78772ad02ad738
nonamich <dimaeromin97@gmail.com>
2024-02-23 23:46:56 +0200

    first-commit

---
+++ pages/+config.h.ts
@@ -0,0 +1,14 @@
+import type { Config } from "vike/types";
+import vikeSolid from "vike-solid/config";
+import Layout from "../layouts/LayoutDefault";
+import Head from "./Head";
+
+// Default config (can be overridden by pages)
+export default {
+  Layout,
+  Head,
+  // <title>
+  title: "My Vike Solid App",
+  ssr: true, // can be removed since `true` is the default
+  extends: vikeSolid,
+} satisfies Config;
---
+++ pages/Head.tsx
@@ -0,0 +1,12 @@
+// Default <head> (can be overridden by pages)
+
+import logoUrl from "../assets/logo.svg";
+
+export default function Head() {
+  return (
+    <>
+      <meta name="description" content="Demo showcasing Vike + Solid" />
+      <link rel="icon" href={logoUrl} />
+    </>
+  );
+}
